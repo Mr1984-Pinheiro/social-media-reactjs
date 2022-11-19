f6fdf555e3ff6d33f6c7a01affdefcd1522f29ef
Carlos Eduardo Tavares Pinheiro <cadutapinheiro@gmail.com>
2022-11-19 09:12:57 -0300

    icons set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Home from "./pages/home/Home";
+import {Person3Icon} from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hi God!</h1>
+      <Home />
+      <Person3Icon />
+
     </div>
   );
 }
