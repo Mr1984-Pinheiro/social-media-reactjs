7171611da38e6722b58f7c1af5673b296e6b7492
CARLOS EDUARDO TAVARES PINHEIRO <73333067+Mr1984-Pinheiro@users.noreply.github.com>
2022-11-21 03:18:01 -0300

    register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,28 @@ import "./register.scss"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+        <div className="card">
+            <div className="left">
+                <h1>Pinheiro Social</h1>
+                <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <span>Do you have an account?</span>
+                <button>Login</button>
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form >
+                    <input type="text" placeholder="Username" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <input type="text" placeholder="Name" />
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
